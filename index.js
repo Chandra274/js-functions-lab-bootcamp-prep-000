6d478c80c804824713d9fa5e5ccfdc155008be4e
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-13 17:01:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo('Margaret')
+
+function happyHolidayTo(festival,name) {
+  console.log(`Happy ${festival}, ${name}!`)
+}
+happyHolidayTo('Halloween', 'Margaret')
+
+function holidayCountdown(festival,days) {
+  console.log(`It's ${days} until ${festival}!`)
+}
+holidayCountdown('Halloween', 20)
